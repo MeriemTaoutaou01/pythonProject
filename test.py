9cbf444e8590c1fe146632366cb96e0be0f54135
MeriemTaoutaou01 <114879510+MeriemTaoutaou01@users.noreply.github.com>
2022-10-19 04:37:40 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-image = Image.open('C:\\Users\\hp\\exo1_meca.png')
-plt.imshow(image)
-plt.show()
-from PIL import ImageDraw
-I1 = ImageDraw.Draw(image)
-I1.text((331, 142), "hello world", fill=(255, 255, 255, 255))
